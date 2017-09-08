6383cd551779801b2cc098b11b31e3bb5feca6aa
tomscotch <tomscotch@web.de>
2017-09-08 09:30:47 +0200

    Fr 8. Sep 09:30:47 CEST 2017

--- ant.sh
+++ ant.sh
@@ -1,1 +1,9 @@
-cd /home/tomscotch/jme3/ ; git pull ; ant -f /home/tomscotch/jme3/BasicGame -Dnb.internal.action.name=rebuild clean jar && rm /home/tomscotch/simple-server-node/myGame.zip &&  zip /home/tomscotch/simple-server-node/myGame.zip /home/tomscotch/jme3/BasicGame/dist/ -r && cd /home/tomscotch/simple-server-node/ && git commit -am "$(date)" && git push resin master # && resin sync --source myGame.zip --destination /opt
+cd /home/tomscotch/jme3/ ;
+git pull ;
+ant -f /home/tomscotch/jme3/BasicGame -Dnb.internal.action.name=rebuild clean jar &&
+rm /home/tomscotch/simple-server-node/data/myGame.zip &
+zip /home/tomscotch/simple-server-node/data/myGame.zip /home/tomscotch/jme3/BasicGame/dist/ -r &&
+cd /home/tomscotch/simple-server-node/ &&
+#git commit -am "$(date)" &&
+#git push resin master
+resin sync --source data --destination /opt
