82aa387d0f6bc8b55b006092689b5e6cafa03c4f
tomscotch <tomscotch@web.de>
2017-11-01 06:18:24 +0100

    Mi 1. Nov 06:18:24 STD 2017

--- ant.sh
+++ ant.sh
@@ -1,3 +1,1 @@
 ant -f BasicGame -Dnb.internal.action.name=rebuild clean jar ;
-rm ../simple-server-node/data/myGame.zip &&
-zip ../simple-server-node/data/myGame.zip BasicGame/dist/ -r
---
+++ zip.sh
@@ -0,0 +1,2 @@
+rm ../simple-server-node/data/myGame.zip &&
+zip ../simple-server-node/data/myGame.zip BasicGame/dist/ -r
