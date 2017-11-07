4732487c9a9f470f5bc61acdb219c720e4673065
TomScotch <tomscotch@localhost.localdomain>
2017-11-07 07:29:02 +0100

    Tue Nov  7 07:29:01 CET 2017

--- GravityBasedPhysics/src/mygame/Main.java
+++ GravityBasedPhysics/src/mygame/Main.java
@@ -21,59 +21,61 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
+    private BulletAppState bas;
+    private final int size = 7;
+    private Geometry core;
+
     @Override
     public void simpleInitApp() {
 
-        BulletAppState bas = new BulletAppState();
+        inputManager.setCursorVisible(false);
+
+        bas = new BulletAppState();
         bas.initialize(stateManager, this);
         stateManager.attach(bas);
         bas.getPhysicsSpace().setGravity(Vector3f.ZERO);
-        Box a = new Box(1, 1, 1);
-        Box b = new Box(2, 2, 2);
-        Box c = new Box(3, 3, 3);
-        Geometry geomA = new Geometry("BoxA", a);
-        Geometry geomB = new Geometry("BoxB", b);
-        Geometry geomC = new Geometry("BoxC", c);
-        Material matA = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Material matB = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Material matC = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        matA.setColor("Color", ColorRGBA.Blue);
-        geomA.setMaterial(matA);
-        matB.setColor("Color", ColorRGBA.Red);
-        geomB.setMaterial(matB);
-        matC.setColor("Color", ColorRGBA.Yellow);
-        geomC.setMaterial(matC);
-        rootNode.attachChild(geomA);
-        rootNode.attachChild(geomB);
-        rootNode.attachChild(geomC);
-        geomA.move(-10, 0, -10);
-        geomB.move(0, 10, -10);
-        geomC.move(0, 0, -10);
-        RigidBodyControl rbcA = new RigidBodyControl(5);
-        RigidBodyControl rbcB = new RigidBodyControl(10);
-        RigidBodyControl rbcC = new RigidBodyControl(500);
-        geomA.addControl(rbcA);
-        geomB.addControl(rbcB);
-        geomC.addControl(rbcC);
-        bas.getPhysicsSpace().add(geomA);
-        bas.getPhysicsSpace().add(geomB);
-        bas.getPhysicsSpace().add(geomC);
-        MyPhysicsControl mpcA = new MyPhysicsControl();
-        MyPhysicsControl mpcB = new MyPhysicsControl();
-        MyPhysicsControl mpcC = new MyPhysicsControl();
-        geomA.addControl(mpcA);
-        geomB.addControl(mpcB);
-        geomC.addControl(mpcC);
+
+        core = addBox(50000);
+        core.move(size / 2, size / 2, size / 2);
+        rootNode.attachChild(core);
+
+        for (int x = 0; x < size; x++) {
+
+            for (int y = 0; y < size; y++) {
+                for (int z = 0; z < size; z++) {
+                    Geometry geom = addBox(5);
+                    geom.move(x, y, z);
+                    rootNode.attachChild(geom);
+                }
+            }
+        }
+
+        getCamera().getLocation().set(0, 0, 50);
+        getCamera().lookAt(core.getLocalTranslation(), Vector3f.UNIT_XYZ);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        //
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+        //
+    }
+
+    private Geometry addBox(int mass) {
+        Box a = new Box(1, 1, 1);
+        Geometry geomA = new Geometry("Box", a);
+        Material matA = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matA.setColor("Color", ColorRGBA.randomColor());
+        geomA.setMaterial(matA);
+        RigidBodyControl rbcA = new RigidBodyControl(mass);
+        geomA.addControl(rbcA);
+        bas.getPhysicsSpace().add(geomA);
+        MyPhysicsControl mpcA = new MyPhysicsControl();
+        geomA.addControl(mpcA);
+        return geomA;
     }
 
 }
